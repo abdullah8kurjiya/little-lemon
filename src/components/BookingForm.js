0ca81fd081216765ef6513707ef12517a784ed61
Abdullah Saad <abdullasaad28@gmail.com>
2023-06-28 03:26:49 +0300

    edit BookingForm

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function BookingForm() {
+    const [book, setBook] = useState ({
+        date:"",
+        time:"",
+        guests:"1",
+        occasion:"",
+    })
     return (
         <form className="booking">
             <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date"/>
+            <input type="date" id="res-date" value={book.date} onChange={(e) => setBook(e.target.value)}/>
             <label htmlFor="res-time">Choose time</label>
-            <select id="res-time ">
+            <select id="res-time " value={book.time} onChange={(e) => setBook(e.target.value)}>
                 <option>17:00</option>
                 <option>18:00</option>
                 <option>19:00</option>
@@ -13,9 +21,9 @@ export default function BookingForm() {
                 <option>22:00</option>
             </select>
             <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+            <input type="number" placeholder="1" min="1" max="10" id="guests" value={book.guests} onChange={(e) => setBook(e.target.value)}/>
             <label htmlFor="occasion">Occasion</label>
-            <select id="occasion">
+            <select id="occasion" value={book.occasion} onChange={(e) => setBook(e.target.value)}>
                 <option>Birthday</option>
                 <option>Anniversary</option>
             </select>
